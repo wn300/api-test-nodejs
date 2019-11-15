25c3906ae6ced545bc5f2106a932948a927bba49
Wilmer Mancera <wilmermancera@MacBook-Pro-de-Wilmer.local>
2019-11-15 14:02:34 -0500

    donwn load excel

--- src/routes/eployees.js
+++ src/routes/eployees.js
@@ -6,7 +6,11 @@ const multipart = require("connect-multiparty");
 const _xlsx = require("node-xlsx");
 const multipartMidleware = multipart();
 
+var json2xls = require('json2xls');
+
 const fs = require("fs");
+
+router.use(json2xls.middleware);
 
 router.get("/api/employees", (req, res) => {
   mysqlConnection.query("select * from employee", (err, rows, fields) => {
@@ -47,6 +51,29 @@ router.post("/api/employees/upload_file", multipartMidleware, (req, res) => {
       console.log(err);
     }
   });
+});
+var jsonArr = [{
+  foo: 'bar',
+  qux: 'moo',
+  poo: 123,
+  stux: new Date()
+},
+{
+  foo: 'bar',
+  qux: 'moo',
+  poo: 345,
+  stux: new Date()
+}];
+
+router.get('/api/employees/file',function(req, res) {
+  mysqlConnection.query("select * from employee", (err, rows, fields) => {
+    if (!err) {
+      res.xls('emploeados.xlsx', rows);
+    } else {
+      console.log(err);
+    }
+  });
+ 
 });
 
 module.exports = router;
